f18f6bf8d6b0170e93a467489abe94ffc826d908
carterols <cjolsen06@gmail.com>
2020-04-22 17:52:55 -0700

    Updates

---
+++ precache-manifest.47f9e61a14025713ab0986cef7cb2856.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be805d821b6a615828b53f4b830f1557",
+    "url": "/valorant-stats/index.html"
+  },
+  {
+    "revision": "3947d02bf0c1eb00543f",
+    "url": "/valorant-stats/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "10f1c9d713d952f0e4a3",
+    "url": "/valorant-stats/static/js/2.587f68cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/valorant-stats/static/js/2.587f68cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3947d02bf0c1eb00543f",
+    "url": "/valorant-stats/static/js/main.25de94d3.chunk.js"
+  },
+  {
+    "revision": "400af3cde2f932acca99",
+    "url": "/valorant-stats/static/js/runtime-main.d8787afd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/valorant-stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
